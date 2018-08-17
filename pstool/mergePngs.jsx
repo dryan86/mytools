dce2e8d960ee943125ea847dbc5fa38cb4ffb9d4
yanweicai <yanweicai@moqikaka.com>
2018-08-17 10:36:59 +0800

    修复脚本

--- pstool/mergePngs.jsx
+++ pstool/mergePngs.jsx
@@ -1,69 +1,476 @@
 ﻿#target photoshop
-$.evalFile ( "C:/jslib/json2.js" ); 
 
-var canvasWidth = 2742;
-var canvasHeight = 3514;
+//  This is a reference implementation. You are free to copy, modify, or
+//  redistribute.
 
-if (app.documents.length > 0) 
-{
-    var myDocument = app.activeDocument;
-    var thePath = myDocument.path;
-	var docName = decodeURI(app.activeDocument.name);
-	docName = docName.substring(0, docName.indexOf("."));
+/*jslint
+    eval, for, this
+*/
 
-    // modify doc size
-    myDocument.resizeCanvas (canvasWidth, canvasHeight, AnchorPosition.TOPLEFT);
-        
-    var jsonInfo = getJsonData(thePath + "/" + docName + ".json");
-    
-    function fixImages()
-    {
-        var layerLen = myDocument.artLayers.length;
-         // work through the array;
-         for (var m = 0; m < layerLen; m++) 
-         {
-            // open smart object; in active document
-            var getLayerToChange = app.activeDocument.artLayers[m];
-            app.activeDocument.activeLayer = getLayerToChange;
-   
-            var tmpName = getLayerToChange.name;
-            tmpName = tmpName.substring(0, tmpName.indexOf("."));
-            var slotInfo = jsonInfo.skins["default"][tmpName][tmpName];
-            
-            var fixx = slotInfo.x - Math.round(slotInfo.width)/2;
-            var fixy = canvasHeight - (slotInfo.y + Math.round(slotInfo.height)/2);
-            getLayerToChange.translate (fixx, fixy);
-         };  
-    };
+/*property
+    JSON, apply, call, charCodeAt, getUTCDate, getUTCFullYear, getUTCHours,
+    getUTCMinutes, getUTCMonth, getUTCSeconds, hasOwnProperty, join,
+    lastIndex, length, parse, prototype, push, replace, slice, stringify,
+    test, toJSON, toString, valueOf
+*/
 
-    function fixDrawOrder()
-    {
-        var layerLen = myDocument.artLayers.length;
 
-        var allLayers = {};
-        
-         // work through the array;
-         for (var m = 0; m < layerLen; m++) 
-         {
-            // open smart object; in active document
-            var getLayerToChange = app.activeDocument.artLayers[m];
-   
-            var tmpName = getLayerToChange.name;
-            tmpName = tmpName.substring(0, tmpName.indexOf("."));
-            allLayers[tmpName] = getLayerToChange;
-         }; 
+// Create a JSON object only if one does not already exist. We create the
+// methods in a closure to avoid creating global variables.
 
-        var preLayer = null;
-         for (var m in allLayers) 
-         {
-             var layer = allLayers[m];
-             if (preLayer != null){
-                 layer.move (preLayer, ElementPlacement.PLACEBEFORE);
-             }
-             preLayer = layer;
-         }
-    };
+if (typeof JSON !== "object") {
+    JSON = {};
+}
+
+(function () {
+    "use strict";
+
+    var rx_one = /^[\],:{}\s]*$/;
+    var rx_two = /\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g;
+    var rx_three = /"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g;
+    var rx_four = /(?:^|:|,)(?:\s*\[)+/g;
+    var rx_escapable = /[\\"\u0000-\u001f\u007f-\u009f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g;
+    var rx_dangerous = /[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g;
+
+    function f(n) {
+        // Format integers to have at least two digits.
+        return (n < 10)
+            ? "0" + n
+            : n;
+    }
+
+    function this_value() {
+        return this.valueOf();
+    }
+
+    if (typeof Date.prototype.toJSON !== "function") {
+
+        Date.prototype.toJSON = function () {
+
+            return isFinite(this.valueOf())
+                ? (
+                    this.getUTCFullYear()
+                    + "-"
+                    + f(this.getUTCMonth() + 1)
+                    + "-"
+                    + f(this.getUTCDate())
+                    + "T"
+                    + f(this.getUTCHours())
+                    + ":"
+                    + f(this.getUTCMinutes())
+                    + ":"
+                    + f(this.getUTCSeconds())
+                    + "Z"
+                )
+                : null;
+        };
+
+        Boolean.prototype.toJSON = this_value;
+        Number.prototype.toJSON = this_value;
+        String.prototype.toJSON = this_value;
+    }
+
+    var gap;
+    var indent;
+    var meta;
+    var rep;
+
+
+    function quote(string) {
+
+// If the string contains no control characters, no quote characters, and no
+// backslash characters, then we can safely slap some quotes around it.
+// Otherwise we must also replace the offending characters with safe escape
+// sequences.
+
+        rx_escapable.lastIndex = 0;
+        return rx_escapable.test(string)
+            ? "\"" + string.replace(rx_escapable, function (a) {
+                var c = meta[a];
+                return typeof c === "string"
+                    ? c
+                    : "\\u" + ("0000" + a.charCodeAt(0).toString(16)).slice(-4);
+            }) + "\""
+            : "\"" + string + "\"";
+    }
+
+
+    function str(key, holder) {
+
+// Produce a string from holder[key].
+
+        var i;          // The loop counter.
+        var k;          // The member key.
+        var v;          // The member value.
+        var length;
+        var mind = gap;
+        var partial;
+        var value = holder[key];
+
+// If the value has a toJSON method, call it to obtain a replacement value.
+
+        if (
+            value
+            && typeof value === "object"
+            && typeof value.toJSON === "function"
+        ) {
+            value = value.toJSON(key);
+        }
+
+// If we were called with a replacer function, then call the replacer to
+// obtain a replacement value.
+
+        if (typeof rep === "function") {
+            value = rep.call(holder, key, value);
+        }
+
+// What happens next depends on the value's type.
+
+        switch (typeof value) {
+        case "string":
+            return quote(value);
+
+        case "number":
+
+// JSON numbers must be finite. Encode non-finite numbers as null.
+
+            return (isFinite(value))
+                ? String(value)
+                : "null";
+
+        case "boolean":
+        case "null":
+
+// If the value is a boolean or null, convert it to a string. Note:
+// typeof null does not produce "null". The case is included here in
+// the remote chance that this gets fixed someday.
+
+            return String(value);
+
+// If the type is "object", we might be dealing with an object or an array or
+// null.
+
+        case "object":
+
+// Due to a specification blunder in ECMAScript, typeof null is "object",
+// so watch out for that case.
+
+            if (!value) {
+                return "null";
+            }
+
+// Make an array to hold the partial results of stringifying this object value.
+
+            gap += indent;
+            partial = [];
+
+// Is the value an array?
+
+            if (Object.prototype.toString.apply(value) === "[object Array]") {
+
+// The value is an array. Stringify every element. Use null as a placeholder
+// for non-JSON values.
+
+                length = value.length;
+                for (i = 0; i < length; i += 1) {
+                    partial[i] = str(i, value) || "null";
+                }
+
+// Join all of the elements together, separated with commas, and wrap them in
+// brackets.
+
+                v = partial.length === 0
+                    ? "[]"
+                    : gap
+                        ? (
+                            "[\n"
+                            + gap
+                            + partial.join(",\n" + gap)
+                            + "\n"
+                            + mind
+                            + "]"
+                        )
+                        : "[" + partial.join(",") + "]";
+                gap = mind;
+                return v;
+            }
+
+// If the replacer is an array, use it to select the members to be stringified.
+
+            if (rep && typeof rep === "object") {
+                length = rep.length;
+                for (i = 0; i < length; i += 1) {
+                    if (typeof rep[i] === "string") {
+                        k = rep[i];
+                        v = str(k, value);
+                        if (v) {
+                            partial.push(quote(k) + (
+                                (gap)
+                                    ? ": "
+                                    : ":"
+                            ) + v);
+                        }
+                    }
+                }
+            } else {
+
+// Otherwise, iterate through all of the keys in the object.
+
+                for (k in value) {
+                    if (Object.prototype.hasOwnProperty.call(value, k)) {
+                        v = str(k, value);
+                        if (v) {
+                            partial.push(quote(k) + (
+                                (gap)
+                                    ? ": "
+                                    : ":"
+                            ) + v);
+                        }
+                    }
+                }
+            }
+
+// Join all of the member texts together, separated with commas,
+// and wrap them in braces.
+
+            v = partial.length === 0
+                ? "{}"
+                : gap
+                    ? "{\n" + gap + partial.join(",\n" + gap) + "\n" + mind + "}"
+                    : "{" + partial.join(",") + "}";
+            gap = mind;
+            return v;
+        }
+    }
+
+// If the JSON object does not yet have a stringify method, give it one.
+
+    if (typeof JSON.stringify !== "function") {
+        meta = {    // table of character substitutions
+            "\b": "\\b",
+            "\t": "\\t",
+            "\n": "\\n",
+            "\f": "\\f",
+            "\r": "\\r",
+            "\"": "\\\"",
+            "\\": "\\\\"
+        };
+        JSON.stringify = function (value, replacer, space) {
+
+// The stringify method takes a value and an optional replacer, and an optional
+// space parameter, and returns a JSON text. The replacer can be a function
+// that can replace values, or an array of strings that will select the keys.
+// A default replacer method can be provided. Use of the space parameter can
+// produce text that is more easily readable.
+
+            var i;
+            gap = "";
+            indent = "";
+
+// If the space parameter is a number, make an indent string containing that
+// many spaces.
+
+            if (typeof space === "number") {
+                for (i = 0; i < space; i += 1) {
+                    indent += " ";
+                }
+
+// If the space parameter is a string, it will be used as the indent string.
+
+            } else if (typeof space === "string") {
+                indent = space;
+            }
+
+// If there is a replacer, it must be a function or an array.
+// Otherwise, throw an error.
+
+            rep = replacer;
+            if (replacer && typeof replacer !== "function" && (
+                typeof replacer !== "object"
+                || typeof replacer.length !== "number"
+            )) {
+                throw new Error("JSON.stringify");
+            }
+
+// Make a fake root object containing our value under the key of "".
+// Return the result of stringifying the value.
+
+            return str("", {"": value});
+        };
+    }
+
+
+// If the JSON object does not yet have a parse method, give it one.
+
+    if (typeof JSON.parse !== "function") {
+        JSON.parse = function (text, reviver) {
+
+// The parse method takes a text and an optional reviver function, and returns
+// a JavaScript value if the text is a valid JSON text.
+
+            var j;
+
+            function walk(holder, key) {
+
+// The walk method is used to recursively walk the resulting structure so
+// that modifications can be made.
+
+                var k;
+                var v;
+                var value = holder[key];
+                if (value && typeof value === "object") {
+                    for (k in value) {
+                        if (Object.prototype.hasOwnProperty.call(value, k)) {
+                            v = walk(value, k);
+                            if (v !== undefined) {
+                                value[k] = v;
+                            } else {
+                                delete value[k];
+                            }
+                        }
+                    }
+                }
+                return reviver.call(holder, key, value);
+            }
+
+
+// Parsing happens in four stages. In the first stage, we replace certain
+// Unicode characters with escape sequences. JavaScript handles many characters
+// incorrectly, either silently deleting them, or treating them as line endings.
+
+            text = String(text);
+            rx_dangerous.lastIndex = 0;
+            if (rx_dangerous.test(text)) {
+                text = text.replace(rx_dangerous, function (a) {
+                    return (
+                        "\\u"
+                        + ("0000" + a.charCodeAt(0).toString(16)).slice(-4)
+                    );
+                });
+            }
+
+// In the second stage, we run the text against regular expressions that look
+// for non-JSON patterns. We are especially concerned with "()" and "new"
+// because they can cause invocation, and "=" because it can cause mutation.
+// But just to be safe, we want to reject all unexpected forms.
+
+// We split the second stage into 4 regexp operations in order to work around
+// crippling inefficiencies in IE's and Safari's regexp engines. First we
+// replace the JSON backslash pairs with "@" (a non-JSON character). Second, we
+// replace all simple value tokens with "]" characters. Third, we delete all
+// open brackets that follow a colon or comma or that begin the text. Finally,
+// we look to see that the remaining characters are only whitespace or "]" or
+// "," or ":" or "{" or "}". If that is so, then the text is safe for eval.
+
+            if (
+                rx_one.test(
+                    text
+                        .replace(rx_two, "@")
+                        .replace(rx_three, "]")
+                        .replace(rx_four, "")
+                )
+            ) {
+
+// In the third stage we use the eval function to compile the text into a
+// JavaScript structure. The "{" operator is subject to a syntactic ambiguity
+// in JavaScript: it can begin a block or an object literal. We wrap the text
+// in parens to eliminate the ambiguity.
+
+                j = eval("(" + text + ")");
+
+// In the optional fourth stage, we recursively walk the new structure, passing
+// each name/value pair to a reviver function for possible transformation.
+
+                return (typeof reviver === "function")
+                    ? walk({"": j}, "")
+                    : j;
+            }
+
+// If the text is not JSON parseable, then a SyntaxError is thrown.
+
+            throw new SyntaxError("JSON.parse");
+        };
+    }
+}());
+
+
+
+
+var defaultSetting = {
+    canvasWidth : 2742,
+    canvasHeight : 3514,
 };
+showDialog ()
+
+function main(){
+    if (app.documents.length > 0) 
+    {
+        var myDocument = app.activeDocument;
+        var thePath = myDocument.path;
+        var docName = decodeURI(app.activeDocument.name);
+        docName = docName.substring(0, docName.indexOf("."));
+
+        // modify doc size
+		var w = UnitValue( defaultSetting.canvasWidth, "px" );
+		var h = UnitValue( defaultSetting.canvasHeight, "px" );
+		myDocument.resizeCanvas( w, h, AnchorPosition.TOPLEFT );
+            
+        var jsonInfo = getJsonData(thePath + "/" + docName + ".json");
+        fixImages();
+        fixDrawOrder();        
+        
+        function fixImages()
+        {
+            var layerLen = myDocument.artLayers.length;
+             // work through the array;
+             for (var m = 0; m < layerLen; m++) 
+             {
+                // open smart object; in active document
+                var getLayerToChange = app.activeDocument.artLayers[m];
+                app.activeDocument.activeLayer = getLayerToChange;
+       
+                var tmpName = getLayerToChange.name;
+                tmpName = tmpName.substring(0, tmpName.indexOf("."));
+                var slotInfo = jsonInfo.skins["default"][tmpName][tmpName];
+                
+                var fixx = slotInfo.x - Math.round(slotInfo.width)/2;
+                var fixy = defaultSetting.canvasHeight - (slotInfo.y + Math.round(slotInfo.height)/2);
+
+                var x = UnitValue( fixx, "px" );
+                var y = UnitValue( fixy, "px" );
+                getLayerToChange.translate (x, y);
+             };  
+        };
+
+        function fixDrawOrder()
+        {
+            var layerLen = myDocument.artLayers.length;
+
+            var allLayers = {};
+            
+             // work through the array;
+             for (var m = 0; m < layerLen; m++) 
+             {
+                // open smart object; in active document
+                var getLayerToChange = app.activeDocument.artLayers[m];
+       
+                var tmpName = getLayerToChange.name;
+                tmpName = tmpName.substring(0, tmpName.indexOf("."));
+                allLayers[tmpName] = getLayerToChange;
+             }; 
+
+            var preLayer = null;
+             for (var m in jsonInfo.slots) 
+             {
+                var curLayer = allLayers[m.name];
+                 if (preLayer != null){
+                     curLayer.move (preLayer, ElementPlacement.PLACEBEFORE);
+                 }
+                preLayer = curLayer;
+             }
+        };
+    };
+}
 
 function getJsonData(filePath){
     var file = new File(filePath);  
@@ -87,5 +494,41 @@ function getJsonData(filePath){
     return obj; 
 }
 
-fixImages();
-fixDrawOrder();
+function hasFilePath() {
+	var ref = new ActionReference();
+	ref.putEnumerated( charIDToTypeID("Dcmn"), charIDToTypeID("Ordn"), charIDToTypeID("Trgt") ); 
+	return executeActionGet(ref).hasKey(stringIDToTypeID('fileReference'));
+}
+
+function showDialog () {
+	if (!hasFilePath()) {
+		alert("File path not found.\nYou need to save the document before continuing.");
+		return;
+	}
+
+	var dialog = new Window("dialog", "Export Layers");
+    
+	var canvasGroup = dialog.add("panel", [0, 0, 180, 50], "Canvas Size");
+	var canvasWidthText = canvasGroup.add("edittext", [10,10,60,30], defaultSetting.canvasWidth); 
+	var canvasHeightText = canvasGroup.add("edittext", [80,10,130,30], defaultSetting.canvasHeight); 
+	canvasWidthText.onChanging = function() {
+        defaultSetting.canvasWidth = Math.round(canvasWidthText.text);
+	};
+	canvasHeightText.onChanging = function() {
+        defaultSetting.canvasHeight = Math.round(canvasHeightText.text);
+	};
+
+	var confirmGroup = dialog.add("group", [0, 0, 180, 50]);
+	var runButton = confirmGroup.add("button", [10, 10, 80, 35], "Ok");
+	var cancelButton = confirmGroup.add("button", [90, 10, 170, 35], "Cancel");
+	cancelButton.onClick = function() { dialog.close(0); return; };
+
+    runButton.onClick = function() {
+        main();
+        dialog.close ();
+	};
+
+	dialog.orientation = "column";
+	dialog.center();
+	dialog.show();
+}
